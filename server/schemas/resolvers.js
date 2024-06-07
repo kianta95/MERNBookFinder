d5d2694cf3bbf5e0f64043802f05e3f4fd6fbeda
Kianta McDuffie <mcduffie.kianta@gmail.com>
2024-06-07 15:08:18 -0400

    updating code for GraphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,65 +1,65 @@
-const { User } = require("../models");
-const { signToken } = require("../utils/auth");
+const { User, Book } = require('../models');
+
+const { signToken,AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    getUser: async (_, { userId }) => {
-      const foundUser = await User.findById(userId);
-      if (!foundUser) {
-        throw new Error("User not found");
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks');
+
+        return userData;
       }
-      return foundUser;
-    },
-    searchBooks: async (_, { title }) => {
-      const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${title}`);
-      const data = await response.json();
-      return data.items ? data.items.map(book => ({
-        bookId: book.id,
-        authors: book.volumeInfo.authors || ['No author to display'],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
-        image: book.volumeInfo.imageLinks?.thumbnail || '',
-      })) : [];
+      throw new AuthenticationError('Not logged in');
     },
   },
   Mutation: {
-    createUser: async (_, { input }) => {
-      const user = await User.create(input);
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (_, { email, password }) => {
-      const user = await User.findOne({
-        $or: [{ username: email }, { email: email }],
-      });
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
       if (!user) {
-        throw new Error("User not found");
+        throw new AuthenticationError('Invalid Login');
       }
+
       const correctPw = await user.isCorrectPassword(password);
+
       if (!correctPw) {
-        throw new Error("Incorrect password");
+        throw new AuthenticationError('Invalid Logi');
       }
+
       const token = signToken(user);
       return { token, user };
     },
-    saveBook: async (_, { userId, bookInput }) => {
-      const updatedUser = await User.findByIdAndUpdate(
-        userId,
-        { $addToSet: { savedBooks: bookInput } },
-        { new: true }
-      );
-      return updatedUser;
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
-    deleteBook: async (_, { userId, bookId }) => {
-      const updatedUser = await User.findByIdAndUpdate(
-        userId,
-        { $pull: { savedBooks: { bookId: bookId } } },
-        { new: true }
-      );
-      if (!updatedUser) {
-        throw new Error("User not found");
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: bookData } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
       }
-      return updatedUser;
+      throw new AuthenticationError('Please login to save a book.');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+      throw new AuthenticationError('Please login to remove a book.');
     },
   },
 };
--- server/server.js
+++ server/server.js
@@ -1,26 +1,50 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { ApolloServer } = require('@apollo/server');
-const { authMiddleware } = require('./utils/auth');
 const { expressMiddleware } = require('@apollo/server/express4');
-const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 const { json } = require('body-parser');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection:true,
 });
+
+
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start().then(() => {
+    console.log('Apollo Server started with introspection enabled:', server.introspection);
+  });
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', json(), expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
+};
+
+// async function to start the server
+startApolloServer();
